50574429a626bbd0e62dde6afdf35b3b3a706aae
Denys209818 <deniskravcuk63@gmail.com>
2024-12-04 19:24:46 +0200

    make build

---
+++ index.33565af2.js
@@ -0,0 +1,2 @@
+document.getElementById("main-form").addEventListener("submit",function(e){e.preventDefault(),e.stopPropagation(),document.querySelector(".main__form-input").value=""});
+//# sourceMappingURL=index.33565af2.js.map
